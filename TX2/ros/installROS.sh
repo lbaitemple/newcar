a88910b97dff3d92ada37c0f7bd8701435f6ca56
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2019-03-29 22:53:59 -0400

    fix key server link
    old link is broken

--- TX2/ros/installROS.sh
+++ TX2/ros/installROS.sh
@@ -87,7 +87,7 @@ sudo apt-add-repository restricted
 # Setup sources.lst
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 # Setup keys
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 0xB01FA116
 # Installation
 tput setaf 2
 echo "Updating apt-get"
